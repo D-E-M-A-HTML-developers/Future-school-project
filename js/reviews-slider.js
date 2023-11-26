e8493c4500e4a9053f437e2737ef8bd3569a9447
Artiom Mezheynikov <artiom.mezheynikov.work@gmail.com>
2023-11-26 22:02:26 +0300

    26.11.2023 add section reviews HTML CSS version1

---
+++ js/reviews-slider.js
@@ -0,0 +1,5 @@
+const sliderItems = document.querySelectorALL('.slider__card');
+const sliderLine = document.querySelector('.slider__line');
+const sliderBtnPrev = document.querySelector('.slider__btn-prev');
+const sliderBtnNext = document.querySelector('.slider__btn-next');
+
