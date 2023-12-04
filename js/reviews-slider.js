c26a57adefa088884ce893c9edf519edebd573e7
Artiom Mezheynikov <artiom.mezheynikov.work@gmail.com>
2023-12-04 20:53:52 +0300

    04.12.2023 add JS slider .section reviews

--- js/reviews-slider.js
+++ js/reviews-slider.js
@@ -1,5 +1,42 @@
-const sliderItems = document.querySelectorALL('.slider__card');
+const sliderItems = document.querySelectorAll('.slider__card');
 const sliderLine = document.querySelector('.slider__line');
 const sliderBtnPrev = document.querySelector('.slider__btn-prev');
 const sliderBtnNext = document.querySelector('.slider__btn-next');
+
+let sliderCount = 0,
+	sliderWidth;
+
+window.addEventListener('resize', showSliderWidth);
+sliderBtnNext.addEventListener('click', switchNextSlide);
+sliderBtnPrev.addEventListener('click', switchPrevSlide);
+
+
+function showSliderWidth() {
+	sliderWidth = document.querySelector('.reviews__slider').offsetWidth;
+	sliderLine.style.width = sliderWidth * sliderItems.length + 'px';
+	sliderItems.forEach(sliderItem => sliderItem.style.width = sliderWidth + "px");
+
+	createRollSlider()
+}
+showSliderWidth();
+function switchNextSlide() {
+	sliderCount++;
+	if (sliderCount >= sliderItems.length) sliderCount = 0;
+	createRollSlider()
+}
+
+function switchPrevSlide() {
+	sliderCount--;
+	if (sliderCount < 0) sliderCount = sliderItems.length - 1;
+	createRollSlider();
+}
+
+function createRollSlider() {
+	sliderLine.style.transform = `translateX(${-sliderCount * sliderWidth}px)`;
+}
+
+// setInterval(() => {
+// 	switchNextSlide()
+// }, 5000);
+
 
