5c22b1b12961e134b9477c73fa35f373d9f5adef
Artiom Mezheynikov <artiom.mezheynikov.work@gmail.com>
2024-04-26 21:54:30 +0300

    23.04.2024 add section accordion v-2 version. desktop version

--- js/reviews-slider.js
+++ js/reviews-slider.js
@@ -12,18 +12,20 @@ sliderBtnPrev.addEventListener("click", switchPrevSlide);
 
 function showSliderWidth() {
 	sliderWidth = document.querySelector(".slider").offsetWidth;
-	if (sliderWidth < 1000) {
-		sliderLine.style.width = sliderWidth * sliderItems.length + "px";
-		sliderItems.forEach((sliderItem) => (sliderItem.style.width = sliderWidth + "px"));
-	} if (sliderWidth < 1920) {
-		sliderWidth = sliderWidth * 0.667;
-		sliderLine.style.width = sliderWidth * sliderItems.length + "px";
-		sliderItems.forEach((sliderItem) => (sliderItem.style.width = sliderWidth + "px"));
-	} else {
-		sliderWidth = sliderWidth * 0.767;
-		sliderLine.style.width = sliderWidth * sliderItems.length + "px";
-		sliderItems.forEach((sliderItem) => (sliderItem.style.width = sliderWidth + "px"));
-	}
+	sliderLine.style.width = sliderWidth * sliderItems.length + "px";
+	sliderItems.forEach((sliderItem) => (sliderItem.style.width = sliderWidth + "px"));
+	// if (sliderWidth < 1000) {
+	// 	sliderLine.style.width = sliderWidth * sliderItems.length + "px";
+	// 	sliderItems.forEach((sliderItem) => (sliderItem.style.width = sliderWidth + "px"));
+	// } if (sliderWidth < 1920) {
+	// 	sliderWidth = sliderWidth * 0.667;
+	// 	sliderLine.style.width = sliderWidth * sliderItems.length + "px";
+	// 	sliderItems.forEach((sliderItem) => (sliderItem.style.width = sliderWidth + "px"));
+	// } else {
+	// 	sliderWidth = sliderWidth * 0.767;
+	// 	sliderLine.style.width = sliderWidth * sliderItems.length + "px";
+	// 	sliderItems.forEach((sliderItem) => (sliderItem.style.width = sliderWidth + "px"));
+	// }
 
 	createRollSlider();
 }
@@ -44,6 +46,56 @@ function createRollSlider() {
 	sliderLine.style.transform = `translateX(${-sliderCount * sliderWidth}px)`;
 }
 
-// setInterval(() => {
-// 	switchNextSlide()
-// }, 5000);
+setInterval(() => {
+	switchNextSlide()
+}, 5000);
+
+
+// ================== edits by egor
+// const sliderItems = document.querySelectorAll('.slider__card')
+// const sliderLine = document.querySelector('.slider__line')
+// const sliderBtnPrev = document.querySelector('.slider__btn-prev')
+// const sliderBtnNext = document.querySelector('.slider__btn-next')
+
+// let currentSlide = 0
+// let sliderWidth
+
+// function initSlider() {
+// 	sliderWidth = document.querySelector('.slider').offsetWidth
+// 	sliderLine.style.width = sliderWidth * sliderItems.length + 'px'
+// 	sliderItems.forEach(item => {
+// 		item.style.width = sliderWidth + 'px'
+// 	})
+// 	showSlide(currentSlide)
+// }
+
+// function showSlide(index) {
+// 	sliderLine.style.transform = `translateX(-${index * sliderWidth}px)`
+// }
+
+// function prevSlide() {
+// 	if (currentSlide > 0) {
+// 		currentSlide--
+// 		showSlide(currentSlide)
+// 	} else {
+// 		currentSlide = sliderItems.length - 1
+// 		showSlide(currentSlide)
+// 	}
+// }
+
+// function nextSlide() {
+// 	currentSlide++
+// 	if (currentSlide >= sliderItems.length) {
+// 		currentSlide = 0
+// 	}
+// 	showSlide(currentSlide)
+// }
+
+// sliderBtnPrev.addEventListener('click', prevSlide)
+// sliderBtnNext.addEventListener('click', nextSlide)
+// window.addEventListener('resize', initSlider)
+
+// initSlider()
+
+
+
